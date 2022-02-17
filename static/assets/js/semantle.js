b4116b6918d1cdf5ea8ddf10665b680cb8f10716
David Turner <novalis@novalis.org>
2022-02-17 08:59:38 -0500

    copyright ontice

--- static/assets/js/semantle.js
+++ static/assets/js/semantle.js
@@ -1,4 +1,12 @@
+/*
+    Copyright (c) 2022, David Turner <novalis@novalis.org>
 
+     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 function $(id) {
     if (id.charAt(0) !== '#') return false;
     return document.getElementById(id.substring(1));
