092ebd059cb16b206ac8ba49c5e2f3fe234ba345
David Turner <novalis@novalis.org>
2022-02-18 22:11:53 -0500

    defensive programming against extremely sinful caches

--- static/assets/js/semantle.js
+++ static/assets/js/semantle.js
@@ -289,7 +289,7 @@ similarity of ${(similarityStory.rest * 100).toFixed(2)}.
                 guess = guess.toLowerCase();
             }
 
-            if (unbritish.hasOwnProperty(guess)) {
+            if (typeof unbritish !== undefined && unbritish.hasOwnProperty(guess)) {
                 guess = unbritish[guess];
             }
 
