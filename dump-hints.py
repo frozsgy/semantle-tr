d0c07163787ec6e12b7d3440f080aad346d66aac
Daniel Holth <dholth@gmail.com>
2022-02-23 09:01:54 -0500

    dump hints in parallel with progress, improve sql

--- dump-hints.py
+++ dump-hints.py
@@ -1,16 +1,25 @@
+# gensim monkeypatch
+import collections.abc
+
+collections.Mapping = collections.abc.Mapping
+
 import gensim.models.keyedvectors as word2vec
 
 import math
 
 import heapq
+import json
 
 from numpy import dot
 from numpy.linalg import norm
 
 import re
-import time
+import tqdm.contrib.concurrent
 
 import code, traceback, signal
+
+# check against all words + phrases in model?
+ALL_WORDS = False
 
 
 def debug(sig, frame):
@@ -66,38 +75,74 @@ with open("words_alpha.txt") as walpha:
 
 print("loaded alpha...")
 
-simple_word = re.compile("^[a-z]*")
+simple_word = re.compile("^[a-z]*$")
 words = []
 for word in model.vocab:
-    #    if simple_word.match(word) and word in allowable_words:
-    words.append(word)
+    if ALL_WORDS or (simple_word.match(word) and word in allowable_words):
+        words.append(word)
 
 hints = {}
+
+secrets = []  # to have length for progress bar
+
 with open("static/assets/js/secretWords.js") as f:
     for line in f.readlines():
         line = line.strip()
         if not '"' in line:
             continue
-        secret = line.strip('",')
-        target_vec = model[secret]
+        secrets.append(line.strip('",'))
 
-        start = time.time()
-        #        syns = synonyms.get(secret) or []
-        nearest = []
-        for i, word in enumerate(words):
-            #            if word in syns:
-            #                continue
-            #            if secret in (synonyms.get(word) or []):
-            #                # yow, asymmetrical!
-            #                continue
-            #            if word in secret or secret in word:
-            #                continue
-            vec = model[word]
-            s = similarity(vec, target_vec)
-            heapq.heappush(nearest, (s, word))
-            if len(nearest) > 1000:
-                heapq.heappop(nearest)
-        nearest.sort()
+
+def find_hints(secret, progress=True):
+    if progress:  # works poorly in parellel
+        worditer = tqdm.tqdm(words, leave=False)
+    else:
+        worditer = words
+
+    target_vec = model[secret]
+
+    #        syns = synonyms.get(secret) or []
+    nearest = []
+
+    for word in worditer:
+        #            if word in syns:
+        #                continue
+        #            if secret in (synonyms.get(word) or []):
+        #                # yow, asymmetrical!
+        #                continue
+        #            if word in secret or secret in word:
+        #                continue
+        vec = model[word]
+        # why not model.wv.similarity(wordA, wordB)?
+        s = similarity(vec, target_vec)
+        heapq.heappush(nearest, (s, word))
+        if len(nearest) > 1000:
+            heapq.heappop(nearest)
+    nearest.sort()
+    return secret, nearest
+
+
+def find_hints_no_progress(secret):
+    return find_hints(secret, progress=False)
+
+
+CONCURRENCY = True
+if CONCURRENCY:
+    # may need to limit concurrency for memory reasons
+    # XXX bug: wraps all results into a list, e.g. won't write any until the very end
+    mapper = tqdm.contrib.concurrent.process_map(
+        find_hints_no_progress, secrets, chunksize=1, total=len(secrets)
+    )
+else:
+    mapper = tqdm((find_hints(secret) for secret in secrets), total=len(secrets))
+
+
+with open("hints.json", "w+") as hints_file:
+    for secret, nearest in mapper:
+        nearest = [(float(score), word) for score, word in nearest]
+        hints_file.write(json.dumps({"word": secret, "neighbors": nearest}))
+        hints_file.write("\n")
+        hints_file.flush()
         hints[secret] = nearest
 
 import pickle
