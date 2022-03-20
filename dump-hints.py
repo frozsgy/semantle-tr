070536daf3d6618e0645e8d01028be54b2820ef1
Mustafa Ozan Alpay <frozsgy@gmail.com>
2022-03-20 14:43:32 +0300

    reformat dump-hints.py using methods from semantle-es

--- dump-hints.py
+++ dump-hints.py
@@ -1,158 +1,57 @@
-# gensim monkeypatch
-import collections.abc
-
-collections.Mapping = collections.abc.Mapping
-
-from functools import partial
 import pickle
+import time
 
 import gensim.models.keyedvectors as word2vec
-
-import math
-
-import heapq
-import json
-
-from numpy import dot
-from numpy.linalg import norm
-
-import re
-import tqdm.contrib.concurrent
-
-from hashlib import sha1
-
-import code, traceback, signal
-
-# check against all words + phrases in model?
-ALL_WORDS = False
+from tqdm import tqdm
 
 
-model = word2vec.KeyedVectors.load_word2vec_format(
-    "../GoogleNews-vectors-negative300.bin", binary=True
-)
+def get_common_words_set():
+    result = set()
+
+    with open("tr_50k.txt") as common_words:
+        for line in common_words:
+            result.add(line.split()[0])
+    return result
 
 
-def make_words():
-    allowable_words = set()
-    with open("words_alpha.txt") as walpha:
-        for line in walpha.readlines():
-            allowable_words.add(line.strip())
+common_words_set = get_common_words_set()
+TOP_N = 1000
 
-    print("loaded alpha...")
+t_word2vec = time.process_time()
+print("loading word2vec file...")
+model = word2vec.KeyedVectors.load_word2vec_format("word2vec/wikipedia-vector.bin", binary=True)
+print(f'done in {time.process_time() - t_word2vec} seconds')
 
-    # The banned words are stored obfuscated because I do not want a giant
-    # list of banned words to show up in my repository.
-    banned_hashes = set()
-    with open("banned.txt") as f:
-        for line in f:
-            banned_hashes.add(line.strip())
+hints = {}
+with open("static/assets/js/secretWords.js", encoding="utf-8") as secret_words:
+    for secret in tqdm(iterable=secret_words.readlines(), desc='generating hints (takes 1~2 minutes to start)'):
+        secret = secret.strip()
+        if not '"' in secret:
+            continue
+        secret = secret.strip('",')
+        # secret might not be in the model vocabulary if we loaded a subset
+        # of the model. Skip generating hints if that's the case
+        if secret not in model.key_to_index:
+            continue
+        # Calculate nearest using KeyedVectors' `most_similar`.
+        # It calculates cosine similarity, which is  what
+        # the original Semantle does.
+        # The first call to `most_similar` is s l o w: the progress
+        # indicator will start moving after a minute or so.
+        # This is _way_ faster than doing a nested "secret x vocab" loop.
+        # Slice up to TOP_N -1 to leave room for the secret word.
+        most_similar = [it for it in model.most_similar(secret, topn=100 * TOP_N) if it[0] in common_words_set][
+                       0:TOP_N - 1]
+        # Nearest must include the secret. `most_similar` doesn't, so we need to add it manually.
+        most_similar.extend([(secret, 1)])
+        if len(most_similar) < TOP_N:
+            raise RuntimeError(
+                f'most_similar has too few common words: {len(most_similar)} after filtering, needs {TOP_N}')
+        # store-hints.py expects a (score, word) tuple
+        nearest = [(item[1], item[0]) for item in most_similar]
+        # store-hints.py relies on nearest's order to get the closest, 10th and 1000th nearby element.
+        nearest.sort()
+        hints[secret] = nearest
 
-    simple_word = re.compile("^[a-z]*$")
-    words = []
-    for word in model.key_to_index:
-        if ALL_WORDS or (simple_word.match(word) and word in allowable_words):
-            h = sha1()
-            h.update(("banned" + word).encode("ascii"))
-            hash = h.hexdigest()
-            if not hash in banned_hashes:
-                words.append(word)
-
-    return words
-
-
-words = make_words()
-
-
-def debug(sig, frame):
-    """Interrupt running process, and provide a python prompt for
-    interactive debugging."""
-    d = {"_frame": frame}  # Allow access to frame object.
-    d.update(frame.f_globals)  # Unless shadowed by global
-    d.update(frame.f_locals)
-
-    i = code.InteractiveConsole(d)
-    message = "Signal received : entering python shell.\nTraceback:\n"
-    message += "".join(traceback.format_stack(frame))
-    i.interact(message)
-
-def find_hints(secret, progress=True):
-    if progress:  # works poorly in parellel
-        worditer = tqdm.tqdm(words, leave=False)
-    else:
-        worditer = words
-
-    target_vec = model[secret]
-    target_vec_norm = norm(target_vec)
-
-    #        syns = synonyms.get(secret) or []
-    nearest = []
-
-    for word in worditer:
-        #            if word in syns:
-        #                continue
-        #            if secret in (synonyms.get(word) or []):
-        #                # yow, asymmetrical!
-        #                continue
-        #            if word in secret or secret in word:
-        #                continue
-        vec = model[word]
-        # why not model.wv.similarity(wordA, wordB)?
-        similarity = dot(vec, target_vec) / (norm(vec) * target_vec_norm)
-        heapq.heappush(nearest, (similarity, word))
-        if len(nearest) > 1000:
-            heapq.heappop(nearest)
-    nearest.sort()
-    return secret, nearest
-
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGUSR1, debug)  # Register handler
-
-    # synonyms = {}
-
-    # with open("moby/words.txt") as moby:
-    #     for line in moby.readlines():
-    #         line = line.strip()
-    #         words = line.split(",")
-    #         word = words[0]
-    #         synonyms[word] = set(words)
-
-    print("loaded moby...")
-
-    hints = {}
-
-    secrets = []  # to have length for progress bar
-
-    with open("static/assets/js/secretWords.js") as f:
-        for line in f.readlines():
-            line = line.strip()
-            if not '"' in line:
-                continue
-            secrets.append(line.strip('",'))
-
-    CONCURRENCY = True
-    if CONCURRENCY:
-        # may need to limit concurrency for memory reasons
-        # XXX bug: wraps all results into a list, e.g. won't write any until the very end
-        mapper = tqdm.contrib.concurrent.process_map(
-            partial(find_hints, progress=False),
-            secrets,
-            max_workers=12,
-            chunksize=1,
-            total=len(secrets),
-        )
-    else:
-        mapper = tqdm.tqdm(
-            (find_hints(secret) for secret in secrets), total=len(secrets)
-        )
-
-    with open("hints.json", "w+") as hints_file:
-        for secret, nearest in mapper:
-            nearest = [(float(score), word) for score, word in nearest]
-            hints_file.write(json.dumps({"word": secret, "neighbors": nearest}))
-            hints_file.write("\n")
-            hints_file.flush()
-            hints[secret] = nearest
-
-    with open(b"nearest.pickle", "wb") as f:
-        pickle.dump(hints, f)
+with open(b"nearest.pickle", "wb") as pickled:
+    pickle.dump(hints, pickled)
